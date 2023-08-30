2211e23a5e5de4eaf27cf1df40bdcd2df7fe9791
cgrisales04 <cristiangrisales04@gmail.com>
2023-08-30 10:52:04 -0500

    Optimizacion de elementos

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
 import VideoItem from "./components/VideoItem/VideoItem";
 import VideoList from "./components/VideoList/VideoList";
 
-const list_course = [
-  {
-    title: "Node Js",
-    duration: "24 hrs",
-    date: "00-00-00",
-    description: "Introducción a Curso de Node JS",
-  },
-];
-
 const App = () => {
+  const list_course = [
+    {
+      title: "Node Js",
+      duration: "24 hrs",
+      date: "00-00-00",
+      description: "Introducción a Curso de Node JS",
+    },
+    {
+      title: "Angular 12",
+      duration: "27 hrs",
+      date: "00-00-00",
+      description: "Introducción a Angular 12",
+    },
+  ];
+  const list_music = [
+    {
+      title: "Normal AZ",
+      duration: "2:45 Min",
+      date: "00-00-00",
+      description: "Song from Alcolirykoz",
+    },
+    {
+      title: "Chango AZ",
+      duration: "3 Min",
+      date: "00-00-00",
+      description: "Song from Alcolirykoz",
+    },
+  ];
   return (
     <div className="container">
       <h2 className="text-center mt-4 mb-4">Categoria de musica</h2>
       <div className="d-flex">
         <VideoList title="Programacion">
-          <VideoItem title="A" duration="" date="" description=""></VideoItem>
+          <VideoItem list_data={list_course}></VideoItem>
         </VideoList>
 
         <VideoList title="Music">
-          <VideoItem title="B" duration="" date="" description=""></VideoItem>
+          <VideoItem list_data={list_music}></VideoItem>
         </VideoList>
       </div>
     </div>
--- src/components/VideoItem/VideoItem.jsx
+++ src/components/VideoItem/VideoItem.jsx
@@ -1,8 +1,14 @@
-const VideoItem = ({ title, duration, date, description }) => {
+const VideoItem = ({ list_data }) => {
+  let videoItemsCourse = list_data.map(
+    ({ title, duration, date, description }, index) => {
+      return <option key={index}>{title}</option>;
+    }
+  );
+
   return (
     <select className="form-select" aria-label="Default select example">
-      <option selected>Open this select menu</option>
-      <option value="">{title}</option>
+      <option value="0" selected disabled> Selecciona una opcion</option>
+      {videoItemsCourse}
     </select>
   );
 };
